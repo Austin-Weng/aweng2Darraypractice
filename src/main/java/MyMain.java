c1280efa82a24256df3b51a6060a2aeefa16083d
Austin Weng <24aweng@athenian.org>
2023-01-10 15:15:09 -0800

    Got mode to work in 2d array yay

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,15 +1,31 @@
+import java.util.ArrayList;
+
 public class MyMain {
 
     // Returns the number of odd numbers in mat
     public static int countOdds(int[][] mat) {
-        // YOUR CODE HERE
-        return -1;
+        int oddCount = 0;
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if (mat[row][col] % 2 != 0) {
+                    oddCount++;
+                }
+            }
+        }
+        return oddCount;
     }
 
     // Returns the largest value in mat
     public static int findLargest(int[][] mat) {
-        // YOUR CODE HERE
-        return -1;
+        int largestNum = Integer.MIN_VALUE;
+        for (int[] row : mat) {
+            for (int cell : row) {
+                if (cell >= largestNum) {
+                    largestNum = cell;
+                }
+            }
+        }
+        return largestNum;
     }
 
     // Write a method that "left-shifts" each row in a 2D array.
@@ -17,15 +33,29 @@ public class MyMain {
     // shifted to the left by 1, with wraparound (the first value
     // should wrap around to the last entry in the row)
     public static int[][] leftShift(int[][] mat) {
-        // YOUR CODE HERE
-        return null;
+        for (int[] row : mat){
+            int first = row[0];
+            for (int i = 0; i < row.length; i++){
+                if (i == row.length-1) {
+                    row[i] = first;
+                } else {
+                    row[i] = row[i+1];
+                }
+            }
+        }
+        return mat;
     }
 
     // Creates a 2D array of size arraySize x arraySize
     // where each value is each to the sum of its row and col indexes
     public static int[][] arrayBuilder(int arraySize) {
-        // YOUR CODE HERE
-        return null;
+        int[][] newArray = new int[arraySize][arraySize];
+        for (int row = 0; row < arraySize; row++){
+            for (int col = 0; col < arraySize; col++){
+                newArray[row][col] = row + col;
+            }
+        }
+        return newArray;
     }
 
 
@@ -41,23 +71,59 @@ public class MyMain {
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        int size = mat.length * mat[0].length;
+        double sum = 0;
+        for (double[] row : mat){
+            for (double col : row){
+                sum += col;
+            }
+        }
+        sum = sum/size;
+        return sum;
     }
 
     // Returns the median of the 2D array mat
     // Remember that the array is sorted!
     public static double median(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        ArrayList<Double> newArr = new ArrayList<Double>();
+        for (double[] row : mat) {
+            for (double col : row){
+                newArr.add(col);
+            }
+        }
+        if (newArr.size() % 2 == 1) {
+            return newArr.get(newArr.size()/2);
+        } else {
+            double first = newArr.get(newArr.size()/2);
+            double last = newArr.get((newArr.size()/2) -1);
+            return (first + last)/2;
+        }
+
     }
 
 
     // Returns the mode of the 2D array mat
     // Remember that the array is sorted!
     public static double mode(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        double num = mat[0][0];
+        int numOfNum = 0;
+        double mostNum = mat[0][0];
+        int mostNumOf = 0;
+        for (double[] row : mat) {
+            for (int col = 0; col < mat[0].length; col++){
+                if (row[col] != num){
+                    if (numOfNum > mostNumOf) {
+                        mostNumOf = numOfNum;
+                        mostNum = row[col-1];
+                    }
+                    num = row[col];
+                    numOfNum = 0;
+                    num = row[col];
+                }
+                numOfNum++;
+            }
+        }
+        return mostNum;
     }
 
 
